6ae40fa7b1ab0199f2216974f0a11827d8ba1583
Keunlas <126337231+keunlas@users.noreply.github.com>
2026-04-28 12:08:00 +0800

    Config install base.hpp

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,6 +1,10 @@
 ### install include ###
 # install(DIRECTORY ${CMAKE_SOURCE_DIR}/include/lib1 DESTINATION include)
 # install(DIRECTORY ${CMAKE_SOURCE_DIR}/include/lib2 DESTINATION include)
+install(FILES
+  ${CMAKE_SOURCE_DIR}/include/base.hpp
+  DESTINATION include
+)
 
 ### install lib ###
 # install(TARGETS target1 DESTINATION lib)
