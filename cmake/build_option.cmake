35557e837ffc750c2372c1f151b86fbfd71196e7
keunlas <126337231+keunlas@users.noreply.github.com>
2026-04-30 17:32:19 +0800

    Add build options

--- cmake/build_option.cmake
+++ cmake/build_option.cmake
@@ -2,6 +2,8 @@
 # Option and Cache Var
 set(MY_LIBRARY_TYPE "STATIC" CACHE STRING "STATIC | SHARED")
 option(MY_POSITION_INDEPENDENT_CODE "Is use -fPIC to compile code." ON)
+option(MY_BUILD_TEST "Is build test codes" OFF)
+option(MY_BUILD_EXAMPLE "Is build test codes" OFF)
 ########################################
 
 # Message for Debug
